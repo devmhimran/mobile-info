6379f6478e6f78c96ee7338d5559f14bc35baa96
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-03-01 05:14:20 +0600

    Connected  With API

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
-console.log('connected');
+
+const searchInput = searchId => {
+    const searchInput = document.getElementById(searchId);
+    const searchInputValue = searchInput.value;
+    searchInput.value = '';
+    return searchInputValue;
+}
+
+document.getElementById('search-button').addEventListener('click', function(){
+    const searchInputText = searchInput('search-input');
+    // console.log(searchInputText);
+    const inputUrl = `https://openapi.programming-hero.com/api/phones?search=${searchInputText}`;
+    fetch(inputUrl)
+    .then(res => res.json())
+    .then(data => displaySearchPhone(data));
+});
+
+const displaySearchPhone = searchPhoneData => {
+    // for( const phoneData of searchPhoneData ){
+    //     console.log(phoneData[0]);
+    // }
+    console.log(searchPhoneData.data[0]);
+}
