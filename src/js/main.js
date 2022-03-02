06e4be5ccb7fb90bcdaa84cfb502f453732940ed
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-03-02 06:12:50 +0600

    Update Modal Bug

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
-
+// Search ID
 const searchInput = searchId => {
     const searchInput = document.getElementById(searchId);
     const searchInputValue = searchInput.value;
     searchInput.value = '';
     return searchInputValue;
 }
-
+// Preloader
 const preloader = (displayStyle) =>{
     const preloaderId = document.getElementById('preloader');
     preloaderId.style.display = displayStyle;
 }
-
+// Search Input
 document.getElementById('search-button').addEventListener('click', function(){
     const searchInputText = searchInput('search-input');
     // console.log(searchInputText);
@@ -20,7 +20,7 @@ document.getElementById('search-button').addEventListener('click', function(){
     .then(data => displaySearchPhone(data));
     preloader('block');
 });
-
+// Error Message Display
 const errorMessage = (displayStyle) =>{
     const errorMessageId = document.getElementById('error-message');
     errorMessageId.style.display = displayStyle;
@@ -54,7 +54,7 @@ const displaySearchPhone = searchPhoneData => {
                                 <p>Released 2021, September 24</p>
                             </div>
                             <div class="view-button">
-                                <button id="view-button" onclick="viewPhoneDetail('${searchData.slug}')" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" href="#exampleModalToggle" role="button">View</button>
+                                <button id="view-button" onclick="viewPhoneDetail('${searchData.slug}')" class="btn btn-outline-primary btn-sm" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">View</button>
                             </div>
                         </div>
                     </div>
@@ -75,31 +75,48 @@ const viewPhoneDetail = (phoneSlug) =>{
 }
 
 const displaySinglePhone = singlePhoneDetail => {
-    console.log(singlePhoneDetail);
+    // console.log(singlePhoneDetail.mainFeatures.sensors.splice(0));
     const modalDiv = document.getElementById('modal-content');
     modalDiv.innerHTML = '';
     const createModal = document.createElement('div');
-    // createModal.classList.add('show-modal');
+    createModal.classList.add('modal-body');
     createModal.innerHTML = `
 
-    <div class="modal-body container">
-    <div class="modal-close-button text-end">
+    <div class="close-button text-end">
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
-    
-    <div class="row">
-        <div class="col-md-5">
-            <img src="${singlePhoneDetail.image}" alt="">
-        </div>
-        <div class="col-md-7">
-            <h3>${singlePhoneDetail.name}</h3>
-            <div class="phone-detail">
-                <p>Brand: ${singlePhoneDetail.brand}</p>
-                <p class="release-date">Release Date: <span>${singlePhoneDetail.brand}</span></p>
+        <div class="row">
+            <div class="col-md-5">
+                <img src="${singlePhoneDetail.image}" alt="">
+            </div>
+            <div class="col-md-7">
+                <h3>${singlePhoneDetail.name}</h3>
+                <div class="phone-short-detail">
+                    <p class="brand">Brand: <span>${singlePhoneDetail.brand}</span></p>
+                    <p class="release-date">Release Date: <span>${singlePhoneDetail.releaseDate}</span></p>
+                </div>
             </div>
         </div>
+    <hr class="my-3">
+    <div class="mainFeatures">
+        <h4>Main Features</h4>
+        <p>Display Size: <span>${singlePhoneDetail.mainFeatures.displaySize}</span></p>
+        <p>Chip Set: <span>${singlePhoneDetail.mainFeatures.chipSet}</span></p>
+        <p>Memory: <span>${singlePhoneDetail.mainFeatures.memory}</span></p>
+        <p>Storage: <span>${singlePhoneDetail.mainFeatures.storage}</span></p>
+        <p class="sensor">Sensors: 
+            <span>
+                ${singlePhoneDetail.mainFeatures.sensors[0]}
+                ${singlePhoneDetail.mainFeatures.sensors[1]}
+                ${singlePhoneDetail.mainFeatures.sensors[2]}
+                ${singlePhoneDetail.mainFeatures.sensors[3]}
+                ${singlePhoneDetail.mainFeatures.sensors[4]}
+                ${singlePhoneDetail.mainFeatures.sensors[4]}
+            </span>
+        </p> 
+
     </div>
-</div>
     `;
+
     modalDiv.appendChild(createModal);
 }
